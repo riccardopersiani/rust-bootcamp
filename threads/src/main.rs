d26036fadb3e79cb6fa1f1c37c7413866889a78f
Riccardo Persiani <r.persiani92@gmail.com>
2025-12-16 16:53:11 +0100

    feat: thread 2

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -19,4 +19,26 @@ fn main() {
     let handle2 = thread::spawn(move || {
         println!("{s}");
     });
+
+    let (tx, rx) = mpsc::channel();
+    let sentences = [
+        "!dlroW olleH".to_owned(),
+        ".tsurT eW tsuR nI".to_owned(),
+        "!ytsuR teG s'teL".to_owned(),
+        "!tsuB ro tsuR".to_owned(),
+    ];
+    for s in sentences {
+        let tx_clone = tx.clone();
+
+        thread::spawn(move || {
+            let reversed: String = s.chars().rev().collect();
+            tx_clone.send(reversed).unwrap();
+        });
+    }
+
+    drop(tx);
+    // rx.recv();
+    for sentence in rx {
+        println!("{sentence}");
+    }
 }
