156175e9fa9f4d1a05f68e1028801cd520776a00
Riccardo Persiani <r.persiani92@gmail.com>
2025-12-17 16:34:43 +0100

    feat: async/await expensive

--- async-await/src/main.rs
+++ async-await/src/main.rs
@@ -1,26 +1,48 @@
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
-    let f = my_function();
-    println!("Hello, world 2!");
-    f.await
-}
+use std::time::Duration;
 
-enum FutureStateMachine {
-    State1,
-    State2,
-    State3,
+use tokio::time::sleep;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let mut handles = vec![];
+
+    for i in 0..2 {
+        let handle = tokio::spawn(async move {
+            my_function(i).await;
+        });
+        handles.push(handle);
+    }
+
+    handles.push(tokio::spawn(async {
+        let _res = tokio::task::spawn_blocking(|| {
+            expensive_comp();
+        })
+        .await;
+    }));
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
 }
 
 // "async" is syntactic sugar
-async fn my_function() {
-    println!("I am an async function");
+async fn my_function(i: i32) {
+    println!("[{i}] - I am an async function ");
     let s1: String = read_from_database().await;
-    println!("first res: {s1}");
+    println!("[{i}] - first res: {s1}");
     let s2: String = read_from_database().await;
-    println!("second res: {s2}");
+    println!("[{i}] - second res: {s2}");
 }
 
 async fn read_from_database() -> String {
+    sleep(Duration::from_millis(50)).await;
     "DB Result".to_owned()
+}
+
+fn expensive_comp() {
+    let mut i = 0;
+    for _ in 0..100_000_000 {
+        i = i + 1;
+    }
+    println!("{i}");
 }
